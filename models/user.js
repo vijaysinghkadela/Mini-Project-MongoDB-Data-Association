9bd4c6cb02e0db25804378e148b066539448080f
vijaysinghkadela <132544545+vijaysinghkadela@users.noreply.github.com>
2024-07-16 13:06:10 +0530

    second commit

--- models/user.js
+++ models/user.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/miniproject').then(() => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('Error connecting to MongoDB', err);
-});
-;
-
+mongoose
+  .connect("mongodb://127.0.0.1:27017/miniproject")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
 const userSchema = mongoose.Schema({
-    username: String,
-    name: String,
-    age: Number,
-    email: String,
-    password: String
-})
+  username: String,
+  name: String,
+  age: Number,
+  email: String,
+  password: String,
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+});
 
-
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
